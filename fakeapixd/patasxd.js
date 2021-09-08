de2f978eb086a6253b2871fe350d9cbc5e5fcf26
Mathewbot <90017808+Mathewbot@users.noreply.github.com>
2021-09-08 12:12:43 -0500

    Update patasxd.js

--- fakeapixd/patasxd.js
+++ fakeapixd/patasxd.js
@@ -1,5 +1,6 @@
 [
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZEY4qQmwAJhKVFlypt0nZNgzrkApOax6vew&usqp=CAU"},
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfyqYAyrxqYswPhrZIfJz_wvS_56-zpPoWqg&usqp=CAU"}, 
-{"result": "https://i.ibb.co/f086Pd3/IMG-20210908-WA0291.jpg"} 
+{"result": "https://i.ibb.co/f086Pd3/IMG-20210908-WA0291.jpg"}, 
+{"result": "https://i.ibb.co/nkjsMh3/Polish-20210908-120803697.png"} 
 ]
