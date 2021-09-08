c94b59445667b060090548015d9bcb41561e2f0d
Mathewbot <90017808+Mathewbot@users.noreply.github.com>
2021-09-08 11:22:41 -0500

    Update patasxd.js

--- fakeapixd/patasxd.js
+++ fakeapixd/patasxd.js
@@ -1,4 +1,5 @@
 [
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZEY4qQmwAJhKVFlypt0nZNgzrkApOax6vew&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfyqYAyrxqYswPhrZIfJz_wvS_56-zpPoWqg&usqp=CAU"}
+{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfyqYAyrxqYswPhrZIfJz_wvS_56-zpPoWqg&usqp=CAU"}, 
+{"result": "https://i.ibb.co/f086Pd3/IMG-20210908-WA0291.jpg"} 
 ]
