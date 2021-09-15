f7010faa4214fce3339a79fb5e2f17ad842deef2
Mathewbot <90017808+Mathewbot@users.noreply.github.com>
2021-09-14 19:23:12 -0500

    Update muslosss.js

--- fakeapixd/muslosss.js
+++ fakeapixd/muslosss.js
@@ -1,7 +1,7 @@
 [
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvMRPeHtlg3wipWxCS055lBMcQpYn2Pogdyw&usqp=CAU"},
 {"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZwjGF4urnf7AI5zVNIaZvbGHtt3u--CzBDw&usqp=CAU"}, 
-{"result": "https://i.ibb.co/zx6zd26/IMG-20210913-WA1293.jpg"}, 
+
 {"result": "https://i.ibb.co/4gbzRj6/IMG-20210913-WA0415.jpg"},
 {"result" : "https://i.ibb.co/KxqfsLJ/IMG-20210914-WA0048.jpg"}, 
 {"result" : "https://i.ibb.co/HYrHNNP/IMG-20210914-WA0049.jpg"}, 
